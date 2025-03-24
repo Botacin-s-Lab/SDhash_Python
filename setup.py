ec95c21a6ca4491ba41a31425b2c8b27b987d9a2
mabonmn <mabonmn2002@gmail.com>
2025-03-24 17:05:24 -0500

    SYNC

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup, find_packages
-import os
 
 setup(
-    name="sdhash_wrapper",
-    version="0.1.3",
+    name="jc_sdhash",
+    version="0.2.1",  # Ensure this is 0.2.0
     packages=find_packages(),
     include_package_data=True,
-    package_data={"sdhash_wrapper": ["sdhash"]},  # Ensure binary is included
+    package_data={"jc_sdhash": ["sdhash"]},
     install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "sdhash-python=sdhash_wrapper.wrapper:SDHash"
-        ],
-    },
     author="Mabon Ninan",
     author_email="mabonmn2002@gmail.com",
-    description="A Python wrapper for SDHash binary.",
+    description="A Python wrapper for the SDHash binary.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Botacin-s-Lab/SDhash_Python",
